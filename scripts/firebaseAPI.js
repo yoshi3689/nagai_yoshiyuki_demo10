64a219908ecd83916ac65b1dea0ed20c26393297
yoshi3689 <70615059+yoshi3689@users.noreply.github.com>
2021-11-09 11:01:52 -0800

    Initial commit

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
+};
+  
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+  
