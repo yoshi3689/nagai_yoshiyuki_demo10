13461172783f6e07b19d155a168ab82fac665f59
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2021-11-11 18:40:42 -0800

    added a functionality to fetch changes in the user preference

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: ,
-  authDomain: ,
-  projectId: ,
-  storageBucket: ,
-  messagingSenderId: ,
-  appId: 
+  apiKey: "AIzaSyAvehv3LH6NtbX4iKgjNNperR3Y1jXxYAM",
+  authDomain: "demo8-yoshiyuki.firebaseapp.com",
+  projectId: "demo8-yoshiyuki",
+  storageBucket: "demo8-yoshiyuki.appspot.com",
+  messagingSenderId: "458560914296",
+  appId: "1:458560914296:web:bac96834bee38dd581be8d"
 };
   
 
