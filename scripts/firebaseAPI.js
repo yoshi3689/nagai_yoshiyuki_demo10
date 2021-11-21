f60d083dcc3e1a56ad7b9777b3c1756b596c4617
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2021-11-21 12:14:47 -0800

    made the details page and a functionality that displays a specific tip detail

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,14 +1,14 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyAvehv3LH6NtbX4iKgjNNperR3Y1jXxYAM",
-  authDomain: "demo8-yoshiyuki.firebaseapp.com",
-  projectId: "demo8-yoshiyuki",
-  storageBucket: "demo8-yoshiyuki.appspot.com",
-  messagingSenderId: "458560914296",
-  appId: "1:458560914296:web:bac96834bee38dd581be8d"
-};
+// // Your web app's Firebase configuration
+// var firebaseConfig = {
+//   apiKey: "AIzaSyAvehv3LH6NtbX4iKgjNNperR3Y1jXxYAM",
+//   authDomain: "demo8-yoshiyuki.firebaseapp.com",
+//   projectId: "demo8-yoshiyuki",
+//   storageBucket: "demo8-yoshiyuki.appspot.com",
+//   messagingSenderId: "458560914296",
+//   appId: "1:458560914296:web:bac96834bee38dd581be8d"
+// };
   
 
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// const app = firebase.initializeApp(firebaseConfig);
+// const db = firebase.firestore();
   
